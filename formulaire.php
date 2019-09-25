3891a8878a00f75429fba3e5230e96febf002729
g18000689 <REmi_0809>
2019-09-25 09:50:01 +0200

    tp2

--- formulaire.php
+++ formulaire.php
@@ -3,15 +3,15 @@ include 'utils.inc.php';
 ?>
 <?php
  start_page('formulaire');
- ?>
+?>
 
 <form>
-<input type = "text" name = "Id" placeholder="Identifiant"><br>
-<input type = "radio" id="male" name="sexe" value="Homme" ><br>
+<input type = "text" name = "Id" placeholder="Identifiant">
+<input type = "radio" id="male" name="sexe" value="Homme" >
 <label for="male">Homme</label>
-<input type = "radio" id="femme" name="sexe" value="Femme" ><br>
+<input type = "radio" id="femme" name="sexe" value="Femme" >
 <label for="femme">Femme</label>
-<input type="text" name="email" placeholder="email"><br>
+<input type="text" name="email" placeholder="email">
 <input type="password" name="mdp" placeholder="Mot de Passe">
 <input type="password" name="verifmdp" placeholder="Verification MDP">
 <input type="text" name="tel" placeholder="Telephone Portable">
