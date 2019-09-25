14e540cda856caf493991d6ea8c8e15ab65aa4ef
g18000689 <REmi_0809>
2019-09-25 10:12:50 +0200

    tp2

--- formulaire.php
+++ formulaire.php
@@ -6,14 +6,24 @@ include 'untils.inc.php';
 ?>
 
 <form action="data-processing.php" method="post">
+    <t>
 <input type = "text" name = "Id" placeholder="Identifiant">
+    </t>
+    <t>
 <input type = "radio" id="male" name="sexe" value="Homme" >
 <label for="male">Homme</label>
 <input type = "radio" id="femme" name="sexe" value="Femme" >
 <label for="femme">Femme</label>
+    </t>
+        <t>
 <input type="text" name="email" placeholder="email">
+        </t>
+            <t>
 <input type="password" name="mdp" placeholder="Mot de Passe">
+            </t>
+                <t>
 <input type="password" name="verifmdp" placeholder="Verification MDP">
+                </t>
 <input type="text" name="tel" placeholder="Telephone Portable">
 <select id="pays" name="pays">
     <option value="">Selectionner un pays</option>
