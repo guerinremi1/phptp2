85b8470ccff68deb596b2d560bcee253ec6390a0
g18000689 <REmi_0809>
2019-10-02 09:01:29 +0200

    tp2

--- test-pass.php
+++ test-pass.php
@@ -1,49 +1,14 @@
 <?php
 require 'utils.inc.php';
-$login = $_POST['login'];
-$pwd = $_POST['pwd'];
-// BD
-$dbLink = mysqli_connect('mysql-skyrod.alwaysdata.net', 'skyrod', '123456789')
-or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
-mysqli_select_db($dbLink , 'steffen_td2')
-or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
-$query = 'SELECT * FROM user WHERE login = \'' . $login . '\'';
-if(!($dbQuery = mysqli_query($dbLink, $query)))
-{
-    echo 'Erreur de requête<br/>';
-    // Affiche le type d'erreur.
-    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
-    // Affiche la requête envoyée.
-    echo 'Requête : ' . $query . '<br/>';
-    exit();
-} else if (isset($_POST['action']) && !empty(trim($login)) && !empty(trim($pwd))) {
-    while ($fetch = mysqli_fetch_assoc($dbQuery)) {
-        if ($fetch['password'] == $pwd) {
-            session_start();
-            $_SESSION['login'] = 'ok';
-            $_SESSION['id'] = $login;
-            $_SESSION['pwd'] = $pwd;
-            if (!($updateQuery = mysqli_query($dbLink, 'UPDATE user SET nbConnections = nbConnections + 1 WHERE login = \'' . $login . '\''))) {
-                echo 'Erreur de requête<br/>';
-                // Affiche le type d'erreur.
-                echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
-                // Affiche la requête envoyée.
-                echo 'Requête : ' . $query . '<br/>';
-                exit();
-            }
-        }
-    }
+$identifiant = htmlspecialchars(filter_input(INPUT_POST , 'identifiant'));
+$password = htmlspecialchars(filter_input(INPUT_POST, 'password'));
+$req = $db->query("SELECT * FROM users WHERE identifiant = '" . $identifiant . "' AND password = '" . sha1($password). "'");
+if ($req->rowCount() == 0){
+    $_SESSION['errors'] = 'Identifiant ou mot de passe invalide !';
+    header("Location:login.php");
 } else {
-    header('Location: login.php?step=ERREUR');
+    $_SESSION['success'] = "Vous êtes connecté ! ";
+    $_SESSION['user'] = $req->fetchAll(PDO::FETCH_ASSOC)[0];
+    header("Location:profil.php");
 }
-if ($_SESSION['login'] == 'ok') {
-    if ($login == 'admin')
-        header('Location: admin.php');
-    else {
-        start_page('Bienvenue');
-        echo '<h1> Bienvenue ' . $_SESSION['id'] . '</h1>' . PHP_EOL;
-        end_page();
-    }
-} else {
-    header('Location: login.php?step=ERREUR');
-}
+?>
